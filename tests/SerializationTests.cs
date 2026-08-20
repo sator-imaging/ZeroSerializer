0d63406318bc567dd8c58dd6bf25d7cc8de778bb
sator-imaging <16752340+sator-imaging@users.noreply.github.com>
2026-08-20 09:59:56 +0900

    fix error

--- tests/SerializationTests.cs
+++ tests/SerializationTests.cs
@@ -249,8 +249,8 @@ public sealed class SerializationTests
         TestAssert.Equal(source.Text, view.Text.ToString(), nameof(view.Text));
         TestAssert.SequenceEqual<int>(source.Values, view.Values, nameof(view.Values));
         TestAssert.Equal(source.OptionalNumber, view.OptionalNumber, nameof(view.OptionalNumber));
-        TestAssert.Equal(source.Child.Identifier, view.Child.Identifier, nameof(view.Child.Identifier));
-        TestAssert.Equal(source.Child.State, view.Child.State, nameof(view.Child.State));
+        TestAssert.Equal(source.Child.Identifier, view.Child?.Identifier ?? -1, nameof(view.Child?.Identifier));
+        TestAssert.Equal(source.Child.State, view.Child?.State ?? ByteState.None, nameof(view.Child?.State));
         TestAssert.Equal(source.Tail, view.Tail, nameof(view.Tail));
         int textFieldOffset = BinaryPrimitives.ReadInt32LittleEndian(buffer.AsSpan(0, 4));
         int valuesFieldOffset = BinaryPrimitives.ReadInt32LittleEndian(buffer.AsSpan(4, 4));
@@ -284,8 +284,8 @@ public sealed class SerializationTests
         TestAssert.Equal(source.Text, view.Text.ToString(), nameof(view.Text));
         TestAssert.SequenceEqual<int>(source.Values, view.Values, nameof(view.Values));
         TestAssert.Equal(source.OptionalNumber, view.OptionalNumber, nameof(view.OptionalNumber));
-        TestAssert.Equal(source.Child.Identifier, view.Child.Identifier, nameof(view.Child.Identifier));
-        TestAssert.Equal(source.Child.State, view.Child.State, nameof(view.Child.State));
+        TestAssert.Equal(source.Child.Identifier, view.Child?.Identifier ?? -1, nameof(view.Child?.Identifier));
+        TestAssert.Equal(source.Child.State, view.Child?.State ?? ByteState.None, nameof(view.Child?.State));
         TestAssert.Equal(source.Tail, view.Tail, nameof(view.Tail));
 
         ReadOnlyMemory<byte> borrowedSerializedMemory = view;
@@ -521,9 +521,9 @@ public sealed class SerializationTests
                 _ = view.Text.Length;
                 _ = view.Values.Length;
                 _ = view.OptionalNumber;
-                FixedClassView childView = view.Child;
-                _ = childView.Identifier;
-                _ = childView.State;
+                FixedClassView? childView = view.Child;
+                _ = childView?.Identifier;
+                _ = childView?.State;
                 _ = view.Tail;
             },
             nameof(VariableRecord));
